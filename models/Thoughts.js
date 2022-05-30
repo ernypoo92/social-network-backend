ac8f0d9bb7fbfaa1efad8ef248db11fbf8aa099f
Aaron Jones <ernypoo92@gmail.com>
2022-05-29 21:01:42 -0600

    wrote index.js and started on thoughts but realised I need the date fromat

---
+++ models/Thoughts.js
@@ -0,0 +1,29 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = 
+
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        max: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+    },
+    {
+        toJSON: {
+        getters: true
+        }
+    }
+)
